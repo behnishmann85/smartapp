f64d9f139aa9072e0e93a3f52a984cde87f8a861
banny <banny@rbitconsultancy.ca>
2025-06-04 17:16:36 +0200

    Empty Template

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, logging
+
+app = Flask(__name__)
+
+# Epic Oauth Connectivity
+FHIR_SERVER_URL = "http://157.245.79.105:8080/fhir"
+
+# SMART config
+FHIR_AUTH_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize"
+FHIR_TOKEN_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
+FHIR_API_URL = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/"
+CLIENT_ID = "ffb905d2-f94d-4cb5-a29c-0b048275f662"
+REDIRECT_URI = "http://localhost:5000/Home"
+SCOPES = "patient/launch,patient/Goal.write,patient/Goal.read,patient/Goal.search,patient/Observation.search,patient/Observation.read,patient/Observation.write,patient/Patient.read"
+
+FHIR_AUTH_URL2 = "https://keycloak.testphysicalactivity.com/realms/physical_activity/protocol/openid-connect/auth"
+FHIR_TOKEN_URL2 = "https://keycloak.testphysicalactivity.com/realms/physical_activity/protocol/openid-connect/token"
+FHIR_API_URL2 = "https://fhir.testphysicalactivity.com/fhir/"
+CLIENT_ID2 = "physical_activity"
+REDIRECT_URI2 = "http://localhost:5000/call-back"
+SCOPES2 = "patient/Patient.read"
+
+
+if __name__ == '__main__':
+    import flaskbeaker
+
+    flaskbeaker.FlaskBeaker.setup_app(app)
+
+    logging.basicConfig(level=logging.DEBUG)
+    #    make_ssl_devcert("cert.pem", host="localhost")
+    #    app.run(debug=True, port=8000, ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=True, port=5000)
+    
+    
+    
